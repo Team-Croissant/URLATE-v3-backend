31f8555d2925725b5af1440dd8e2dee749194e17
Hyeokjin Kang <myrhydevelopteam@gmail.com>
2019-11-13 21:55:06 +0900

    UI버그 수정 및 UX버그 수정

--- views/js/join.js
+++ views/js/join.js
@@ -3,7 +3,6 @@ const passReg = /^[0-9]{4,6}$/;
 
 $("#nickname").blur(() => {
     if(!nameReg.test($("#nickname").val())) {
-        alert("닉네임(별명)은 5글자에서 12글자의 문자열(A-Z, a-z, _, -)로 이루어져야 합니다.");
         $("#name").fadeIn(500);
     } else {
         $("#name").fadeOut(500);
@@ -12,9 +11,21 @@ $("#nickname").blur(() => {
 
 $("#password").blur(() => {
     if(!passReg.test($("#password").val())) {
-        alert("2차 비밀번호는 0부터 9까지의 숫자 4자리로 이루어져야 합니다.");
         $("#pw").fadeIn(500);
     } else {
         $("#pw").fadeOut(500);
     }
 });
+
+function check() {
+    if(!nameReg.test($("#nickname").val())) {
+        $("#name").fadeIn(500);
+    } else {
+        $("#name").fadeOut(500);
+        if(!passReg.test($("#password").val())) {
+            $("#pw").fadeIn(500);
+        } else {
+            $("#pw").fadeOut(500);
+        }
+    }
+}
