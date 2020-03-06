4ee1de37e5d2fb38e0d911dd319723077b13ae4b
Hyeokjin Kang <myrhydevelopteam@gmail.com>
2020-03-06 13:37:39 +0900

    editor의 jQuery를 제거

--- public/js/editor.js
+++ public/js/editor.js
@@ -2,40 +2,37 @@ const settingApply = () => {
   Howler.volume(settings.sound.musicVolume / 100);
 };
 
-$(document).ready(() => {
-  $.ajax({
-    type: 'GET',
-    url: `${api}/getStatus`,
-    dataType: 'JSON',
-    xhrFields: {
-      withCredentials: true
-    },
-    success: (data => {
-      if(data.status == "Not logined") {
-        window.location.href = `${url}`;
-      } else if(data.status == "Not authorized") {
-        window.location.href = `${url}/authorize`;
-      } else if(data.status == "Not registered") {
-        window.location.href = `${url}/join`;
-      } else {
-        $.ajax({
-          type: 'GET',
-          url: `${api}/getUser`,
-          dataType: 'JSON',
-          xhrFields: {
-            withCredentials: true
-          },
-          complete: (res) => {
-            res = res.responseJSON;
-            settings = JSON.parse(res.settings);
-            settingApply();
-          },
-          error: (err) => {
-            alert(`Error ocurred.\n${err}`);
-            window.location.href = `${url}`;
-          }
-        });
-      }
-    })
+document.addEventListener("DOMContentLoaded", (event) => {
+  fetch(`${api}/getStatus`, {
+    method: 'GET',
+    credentials: 'include'
+  })
+  .then(res => res.json())
+  .then((data) => {
+    if(data.status == "Not authorized") {
+      window.location.href = `${url}/authorize`;
+    } else if(data.status == "Not registered") {
+      window.location.href = `${url}/join`;
+    } else if(data.status == "Not logined") {
+      window.location.href = url;
+    } else {
+      fetch(`${api}/getUser`, {
+        method: 'GET',
+        credentials: 'include'
+      })
+      .then(res => res.json())
+      .then((data) => {
+        if(data.result == 'success') {
+          settings = JSON.parse(data.settings);
+          settingApply();
+        } else {
+          alert(`Error occured.\n${data.description}`);
+        }
+      }).catch((error) => {
+        alert(`Error occured.\n${error}`);
+      });
+    }
+  }).catch((error) => {
+    alert(`Error occured.\n${error}`);
   });
 });
