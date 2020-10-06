6130c664e2d2deaa96ee0db3a6d26b687f3684f9
HyeokjinKang <coupy@croissant.tech>
2020-10-06 12:42:23 +0000

    노래를 암호화하고 클라이언트에게 전송하는 cdn서버를 구현하였습니다.

--- dist/cdn/index.js
+++ dist/cdn/index.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var signale = require("signale");
 var http = require("http");
 var express = require("express");
+var CryptoJS = require("crypto-js");
+var fs = require("fs");
 var config = require(__dirname + '/../../config/config.json');
 var app = express();
 app.locals.pretty = true;
@@ -13,6 +15,21 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/../../resources/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.get('/tracks/:quality/:songName', function (req, res) {
+    var song = fs.readFileSync(__dirname + ("/../../resources/private/tracks/" + req.params.quality + "/" + req.params.songName));
+    res.end(song);
+});
+app.get('/getTrack/:quality/:songName', function (req, res) {
+    var song = fs.readFileSync(__dirname + ("/../../resources/private/tracks/" + req.params.quality + "/" + req.params.songName));
+    var arrayBuffer = Uint8Array.from(song).buffer;
+    var key = "1234567887654321";
+    var wordArray = CryptoJS.lib.WordArray.create(arrayBuffer);
+    var encrypted = CryptoJS.AES.encrypt(wordArray, key).toString();
+    res.json({
+        result: "success",
+        data: encrypted
+    });
+});
 http.createServer(app).listen(port, function () {
     signale.success("CDN Server running at port " + port + ".");
 });
--- src/cdn/index.ts
+++ src/cdn/index.ts
@@ -3,6 +3,8 @@ import cookieParser = require('cookie-parser');
 import signale = require('signale');
 import http = require('http');
 import express = require('express');
+import CryptoJS = require('crypto-js');
+import fs = require('fs');
 
 const config = require(__dirname + '/../../config/config.json');
 
@@ -14,6 +16,23 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/../../resources/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.get('/tracks/:quality/:songName', (req, res) => {
+  let song = fs.readFileSync(__dirname + `/../../resources/private/tracks/${req.params.quality}/${req.params.songName}`);
+  res.end(song);
+});
+
+app.get('/getTrack/:quality/:songName', (req, res) => {
+  let song = fs.readFileSync(__dirname + `/../../resources/private/tracks/${req.params.quality}/${req.params.songName}`);
+  let arrayBuffer = Uint8Array.from(song).buffer;
+  let key = "1234567887654321";
+  let wordArray = CryptoJS.lib.WordArray.create(arrayBuffer);
+  let encrypted = CryptoJS.AES.encrypt(wordArray, key).toString();
+  res.json({
+    result: "success",
+    data: encrypted
+  });
+});
 
 http.createServer(app).listen(port, () => {
   signale.success(`CDN Server running at port ${port}.`);
