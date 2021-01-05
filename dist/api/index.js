7e845aa22e86dcba7c305d1454d9daf4f49f502c
HyeokjinKang <coupy@croissant.tech>
2021-01-05 09:11:47 +0000

    유저 로그인 시도 로그에 시간을 같이 띄웁니다.

--- dist/api/index.js
+++ dist/api/index.js
@@ -133,6 +133,7 @@ app.post('/auth/login', function (req, res) {
                 });
             }
             else {
+                signale.debug(new Date());
                 signale.debug("User login blocked by whitelist : " + response.data.emails[0].value);
                 res.status(400).json(api_response_1.createErrorResponse('failed', 'Not Whitelisted', 'Provided email is not whitelisted.'));
             }
