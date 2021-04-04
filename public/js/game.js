3d6989ef7fd53611fed62522827a5c6dfaf130ee
HyeokjinKang <bjgumsun@dimigo.hs.kr>
2021-04-04 23:37:26 +0000

    경고창이 깜빡이는 문제를 해결했습니다.

--- public/js/game.js
+++ public/js/game.js
@@ -350,12 +350,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
   }, 3000);
   if (iniMode == 0) {
     //no intro
-    warningContainer.style.display = "none";
     intro1container.style.display = "none";
     intro2container.style.display = "none";
     loaded++;
     display = 0;
   } else {
+    warningContainer.style.display = "flex";
     if (intro1video.canPlayType("video/webm")) {
       intro1sources.src = "videos/croissant.webm";
     } else {
