b7203158e703197aaef769b989772279ef9f82fc
HyeokjinKang <bjgumsun@dimigo.hs.kr>
2021-04-13 03:39:19 +0000

    매 틱마다 총알판정을 검증하는 로직을 만들었습니다.

--- src/game/index.ts
+++ src/game/index.ts
@@ -37,19 +37,57 @@ const getPatternDir = (name, difficulty) => {
 };
 
 const redisGet = (key) => {
-  return new Promise(function (resolve, reject) {
+  return new Promise((resolve, reject) => {
     redisClient.get(key, (err, data) => {
       resolve(data);
     });
   });
 };
 
-const redisSGet = (key) => {
-  return new Promise(function (resolve, reject) {
+const redisSGet = (key): Promise<number[]> => {
+  return new Promise((resolve, reject) => {
     redisClient.smembers(key, (err, data) => {
       resolve(data);
     });
   });
+};
+
+const getTan = (deg) => {
+  let rad = (deg * Math.PI) / 180;
+  return Math.tan(rad);
+};
+
+const calcAngleDegrees = (x, y) => {
+  return (Math.atan2(y, x) * 180) / Math.PI;
+};
+
+const lowerBound = (array, value) => {
+  if (value < 0) value = 0;
+  let low = 0;
+  let high = array.length;
+  while (low < high) {
+    const mid = Math.floor(low + (high - low) / 2);
+    if (value <= array[mid].ms) {
+      high = mid;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return low;
+};
+
+const upperBound = (array, value) => {
+  let low = 0;
+  let high = array.length;
+  while (low < high) {
+    const mid = Math.floor(low + (high - low) / 2);
+    if (value >= array[mid].ms) {
+      low = mid + 1;
+    } else {
+      high = mid;
+    }
+  }
+  return low;
 };
 
 redisClient.on("error", function (error) {
@@ -66,10 +104,16 @@ io.on("connection", (socket) => {
       fs.readFile(
         patternDir + getPatternDir(name, difficulty),
         "utf8",
-        (err, data) => {
-          redisClient.set(`pattern${socket.id}`, data);
+        (err, file) => {
+          let data = JSON.parse(file);
+          redisClient.set(`pattern${socket.id}`, file);
+          redisClient.set(`bpm${socket.id}`, data.information.bpm);
+          redisClient.set(`speed${socket.id}`, data.information.speed);
         }
       );
+      redisClient.set(`prevDate${socket.id}`, 0);
+      redisClient.set(`prevX${socket.id}`, 0);
+      redisClient.set(`prevY${socket.id}`, 0);
       redisClient.set(`score${socket.id}`, 0);
       redisClient.set(`combo${socket.id}`, 0);
       redisClient.del(`destroyedBullets${socket.id}`);
@@ -96,12 +140,123 @@ io.on("connection", (socket) => {
     signale.start(`${socket.id} : Game Resume`);
   });
 
-  socket.on("game update", (x, y, offset, date) => {
-    let seek;
-    redisClient.get(`ms${socket.id}`, async (err, ms) => {
-      seek = date - ms - offset;
-      console.log(seek, x, y);
-    });
+  socket.on("game update", async (mouseX, mouseY, offset, date) => {
+    const prevX = Number(await redisGet(`prevX${socket.id}`));
+    const prevY = Number(await redisGet(`prevY${socket.id}`));
+    const prevDate = Number(await redisGet(`prevDate${socket.id}`));
+    const ms = Number(await redisGet(`ms${socket.id}`));
+    let bpm = Number(await redisGet(`bpm${socket.id}`));
+    let speed = Number(await redisGet(`speed${socket.id}`));
+    let pattern = await JSON.parse(`${await redisGet(`pattern${socket.id}`)}`);
+    let destroyedBullets: number[] = await redisSGet(
+      `destroyedBullets${socket.id}`
+    );
+    destroyedBullets = destroyedBullets.map(Number);
+    let circleBulletAngles = await redisGet(`circleBulletAngles${socket.id}`);
+    circleBulletAngles = `${circleBulletAngles}`.split(",").map(Number);
+    for (let s = 1; s <= 10; s++) {
+      const x = prevX + ((mouseX - prevX) / 10) * s;
+      const y = prevY + ((mouseY - prevY) / 10) * s;
+      const seek = prevDate + ((date - prevDate) / 10) * s - ms - offset;
+      let start = lowerBound(pattern.triggers, 0);
+      let end = upperBound(pattern.triggers, seek);
+      const renderTriggers = pattern.triggers.slice(start, end);
+      for (let i = 0; i < renderTriggers.length; i++) {
+        if (renderTriggers[i].value == 0) {
+          if (destroyedBullets.indexOf(renderTriggers[i].num) == -1) {
+            await destroyedBullets.push(renderTriggers[i].num);
+            redisClient.sadd(
+              `destroyedBullets${socket.id}`,
+              renderTriggers[i].num
+            );
+            signale.warn(`${socket.id} : Bullet`);
+          }
+        } else if (renderTriggers[i].value == 1) {
+          end = upperBound(pattern.bullets, renderTriggers[i].ms);
+          const renderBullets = pattern.bullets.slice(0, end);
+          for (let j = 0; renderBullets.length > j; j++) {
+            if (destroyedBullets.indexOf(j) == -1) {
+              destroyedBullets.push(j);
+              redisClient.set(`combo${socket.id}`, 0);
+              redisClient.sadd(`destroyedBullets${socket.id}`, j);
+            }
+          }
+        } else if (renderTriggers[i].value == 2) {
+          bpm = renderTriggers[i].bpm;
+          redisClient.set(`bpm${socket.id}`, renderTriggers[i].bpm);
+        } else if (renderTriggers[i].value == 4) {
+          speed = renderTriggers[i].speed;
+          redisClient.set(`speed${socket.id}`, renderTriggers[i].speed);
+        }
+      }
+      start = lowerBound(pattern.bullets, seek - bpm * 100);
+      end = upperBound(pattern.bullets, seek);
+      const renderBullets = pattern.bullets.slice(start, end);
+      for (let i = 0; i < renderBullets.length; i++) {
+        const e = renderBullets[i];
+        if (destroyedBullets.indexOf(start + i) == -1) {
+          const p = ((seek - e.ms) / ((bpm * 40) / speed / e.speed)) * 100;
+          let left = e.direction == "L";
+          let ex = (left ? -1 : 1) * (100 - p);
+          let ey = 0;
+          if (e.value == 0) {
+            ey = e.location + p * getTan(e.angle) * (left ? 1 : -1);
+          } else {
+            if (!circleBulletAngles[start + i]) {
+              circleBulletAngles[start + i] = calcAngleDegrees(
+                (left ? -100 : 100) - x,
+                e.location - y
+              );
+              redisClient.set(
+                `circleBulletAngles${socket.id}`,
+                circleBulletAngles.toString()
+              );
+            }
+            if (left) {
+              if (
+                110 > circleBulletAngles[start + i] &&
+                circleBulletAngles[start + i] > 0
+              )
+                circleBulletAngles[start + i] = 110;
+              else if (
+                0 > circleBulletAngles[start + i] &&
+                circleBulletAngles[start + i] > -110
+              )
+                circleBulletAngles[start + i] = -110;
+            } else {
+              if (
+                70 < circleBulletAngles[start + i] &&
+                circleBulletAngles[start + i] > 0
+              )
+                circleBulletAngles[start + i] = 70;
+              else if (
+                0 > circleBulletAngles[start + i] &&
+                circleBulletAngles[start + i] < -70
+              )
+                circleBulletAngles[start + i] = -70;
+            }
+            ey =
+              e.location +
+              p * getTan(circleBulletAngles[start + i]) * (left ? 1 : -1);
+          }
+          const powX = (x - ex) * 9.6;
+          const powY = (y - ey) * 9.6;
+          if (Math.sqrt(Math.pow(powX, 2) + Math.pow(powY, 2)) <= 24) {
+            if (destroyedBullets.indexOf(start + i) == -1) {
+              await destroyedBullets.push(start + i);
+              redisClient.set(`combo${socket.id}`, 0);
+              redisClient.sadd(`destroyedBullets${socket.id}`, start + i);
+              signale.warn(`${socket.id} : Bullet`);
+            }
+          }
+        }
+      }
+    }
+    redisClient.set(`prevDate${socket.id}`, date);
+    redisClient.set(`prevX${socket.id}`, mouseX);
+    redisClient.set(`prevY${socket.id}`, mouseY);
+  });
+
   });
 
   socket.on("game end", () => {
