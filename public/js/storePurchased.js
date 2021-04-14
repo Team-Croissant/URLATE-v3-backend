ad6cf5e85c8bee7cbb14347d9e037c11ec08cbe8
HyeokjinKang <bjgumsun@dimigo.hs.kr>
2021-04-14 02:11:59 +0000

    결제 결과 화면에서 game으로 넘어갈 때 경고창이 다시 나타나던 문제를 해결했습니다.

--- public/js/storePurchased.js
+++ public/js/storePurchased.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   setInterval(() => {
     infoSecondText.textContent = infoSecondText.textContent - 1;
     if (infoSecondText.textContent == 0) {
-      window.location.href = `${url}/game`;
+      window.location.href = `${url}/game?initialize=0`;
     }
   }, 1000);
 });
