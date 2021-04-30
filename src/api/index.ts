7d2e222f5103b5d42a4903ec1254804d144a788a
HyeokjinKang <bjgumsun@dimigo.hs.kr>
2021-04-30 08:14:31 +0000

    화이트리스트 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -19,7 +19,7 @@ const OAuth2 = google.auth.OAuth2;
 const plus = google.plus("v1");
 
 let whitelist =
-  "bjgumsun@gmail.com, bjgumsun@dimigo.hs.kr, kyungblog@gmail.com, pop06296347@gmail.com, combbm@gmail.com, jeongjy0317@gmail.com, electrochemistry04@gmail.com, jungin7612@gmail.com";
+  "bjgumsun@gmail.com, bjgumsun@dimigo.hs.kr, kyungblog@gmail.com, pop06296347@gmail.com, combbm@gmail.com, jeongjy0317@gmail.com, electrochemistry04@gmail.com, jungin7612@gmail.com, greenstar1151@gmail.com, kiwiyou.dev@gmail.com";
 
 import {
   createSuccessResponse,
