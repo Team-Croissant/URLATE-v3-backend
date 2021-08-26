913b937264b205ab7c966facf04b99d730404185
RanolP <public.ranolp@gmail.com>
2021-08-26 19:38:11 +0900

    타입스크립트 사용 경험 개선
    @types/* 패키지 설치 및 express-session.d.ts를 만들어
    세션에서 접근할 수 있는 값들을 타입화했습니다.

---
+++ typings/express-session.d.ts
@@ -0,0 +1,15 @@
+declare module 'express-session' {
+  interface SessionData {
+    accessToken: unknown;
+    refreshToken: unknown;
+    userid: unknown;
+    tempName: unknown;
+    email: unknown;
+    authorized: unknown;
+    bag: unknown[];
+    TID: unknown;
+    vaildChecked: unknown;
+  }
+}
+
+export {};
